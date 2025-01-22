625230d1676a3f5f310448a82237afbe7592c626
Tong-man <92763036+TCM14@users.noreply.github.com>
2025-01-22 20:01:36 +0800

    Update ESP32-1.ino

--- ESP32-1.ino
+++ ESP32-1.ino
@@ -65,9 +65,65 @@ float BLErssi = 0.0;
 std::vectorstd::string knownBLEAddresses = { "a4:c1:38:xx:xx:xx", "a4:c1:38:xx:xx:xx" }; 
 ATC_MiThermometer miThermometer(knownBLEAddresses);
 
-void setup() { Serial.begin(115200); initWifiStation(); initMQTTClient(); delay(500); miThermometer.begin(); loopCount = 180; PrevMillis = millis(); }
+void setup() { 
+  Serial.begin(115200); 
+  initWifiStation();
+  initMQTTClient();
+  delay(500); 
+  miThermometer.begin();
+  loopCount = 180;
+  PrevMillis = millis(); 
+}
 
-void loop() { mqttClient.loop(); delay(100); //Leave some time for mqttClient.loop to process CallBack function loopCount++; if (loopCount >= 6000 && (millis() - PrevMillis) >= 3600000) { loopCount = 0; PrevMillis = millis(); retries++; initMQTTClient(); tp = "0.0"; hp = "0.0"; vp = "0.0"; pp = "0.0"; sp = "0.0"; tp2 = "0.0"; hp2 = "0.0"; vp2 = "0.0"; pp2 = "0.0"; sp2 = "0.0"; } if (loopCount >= 200 & (millis() - PrevMillis) >= 30000) { // Check sensor data every 30,000ms PrevMillis = millis(); miThermometer.resetData(); unsigned found = miThermometer.getData(scanTime); for (int i = 0; i < miThermometer.data.size(); i++) { if (miThermometer.data[i].valid) { delay(50); Serial.printf("i = %d, ", i); Serial.printf("Temp(°C):%.2f, ", miThermometer.data[i].temperature / 100.0); Temperature = miThermometer.data[i].temperature / 100.0; Serial.printf("RH(%%):%.2f, ", miThermometer.data[i].humidity / 100.0); Humidity = miThermometer.data[i].humidity / 100.0; Serial.printf("Volt:%.3f, ", miThermometer.data[i].batt_voltage / 1000.0); Voltage = miThermometer.data[i].batt_voltage / 1000.0; char sbuff[8]; dtostrf(Voltage, 4, 3, sbuff); Serial.printf("batt(%%):%d, ", miThermometer.data[i].batt_level); Percent = miThermometer.data[i].batt_level; Serial.printf("rssi(dBm):%d\n", miThermometer.data[i].rssi); BLErssi = miThermometer.data[i].rssi; if (i < 1) { t = String(Temperature); t = t.substring(0, 5); h = String(Humidity); h = h.substring(0, 5); v = String(sbuff); v = v.substring(0, 5); p = String(round(Percent)); p = p.substring(0, 3); s = String(BLErssi); if (BLErssi > -99) { s = s.substring(0, 3); } else { s = s.substring(0, 4); } } else { t2 = String(Temperature); t2 = t2.substring(0, 5); h2 = String(Humidity); h2 = h2.substring(0, 5); v2 = String(sbuff); v2 = v2.substring(0, 5); p2 = String(round(Percent)); p2 = p2.substring(0, 3); s2 = String(BLErssi); if (BLErssi > -99) { s2 = s2.substring(0, 3); } else { s2 = s2.substring(0, 4); } } delay(50); } } miThermometer.clearScanResults(); if (mqttClient.connected()) retries = 0; // Below lines compare the current BLE value h with previous value hp, h: humidity, t: temperature, etc... if (mqttClient.connected() and ((h.toFloat() >= hp.toFloat() * 1.01) or (h.toFloat() <= hp.toFloat() * 0.99))) { mqttClient.publish(SUB_RH, (char*)h.c_str(), true); hp = h; } if (mqttClient.connected() and ((t.toFloat() >= tp.toFloat() + 0.01)) or (t.toFloat() <= tp.toFloat() - 0.01)) { mqttClient.publish(SUB_TEMP, (char*)t.c_str(), true); tp = t; } if (mqttClient.connected() and ((v.toFloat() >= vp.toFloat() + 0.01)) or (v.toFloat() <= vp.toFloat() - 0.01)) { mqttClient.publish(SUB_V, (char*)v.c_str(), true); vp = v; } if (mqttClient.connected() and (p.toFloat() != pp.toFloat())) { mqttClient.publish(SUB_P, (char*)p.c_str(), true); pp = p; } if (mqttClient.connected() and (s.toFloat() != sp.toFloat())) { mqttClient.publish(SUB_S, (char*)s.c_str(), true); sp = s; } if (mqttClient.connected() and ((h2.toFloat() >= hp2.toFloat() * 1.01) or (h2.toFloat() <= hp2.toFloat() * 0.99))) { mqttClient.publish(SUB_RH2, (char*)h2.c_str(), true); hp2 = h2; } if (mqttClient.connected() and ((t2.toFloat() >= tp2.toFloat() + 0.01)) or (t2.toFloat() <= tp2.toFloat() - 0.01)) { mqttClient.publish(SUB_TEMP2, (char*)t2.c_str(), true); tp2 = t2; } if (mqttClient.connected() and ((v2.toFloat() >= vp2.toFloat() + 0.01)) or (v2.toFloat() <= vp2.toFloat() - 0.01)) { mqttClient.publish(SUB_V2, (char*)v2.c_str(), true); vp2 = v2; } if (mqttClient.connected() and (p2.toFloat() != pp2.toFloat())) { mqttClient.publish(SUB_P2, (char*)p2.c_str(), true); pp2 = p2; } if (mqttClient.connected() and (s2.toFloat() != sp2.toFloat())) { mqttClient.publish(SUB_S2, (char*)s2.c_str(), true); sp2 = s2; } delay(100); //delay some time for MQTT to publish and callback } }
+void loop() { 
+mqttClient.loop();
+delay(100); //Leave some time for mqttClient.loop to process CallBack function 
+loopCount++; 
+if (loopCount >= 6000 && (millis() - PrevMillis) >= 3600000) {
+  loopCount = 0; 
+  PrevMillis = millis();
+  retries++; initMQTTClient();
+  tp = "0.0"; hp = "0.0"; vp = "0.0"; pp = "0.0"; sp = "0.0"; tp2 = "0.0"; hp2 = "0.0"; vp2 = "0.0"; pp2 = "0.0"; sp2 = "0.0";
+} 
+ // Check sensor data every 30,000ms 
+if (loopCount >= 200 & (millis() - PrevMillis) >= 30000) {
+  PrevMillis = millis(); 
+  miThermometer.resetData();
+  unsigned found = miThermometer.getData(scanTime); 
+  for (int i = 0; i < miThermometer.data.size(); i++) { 
+    if (miThermometer.data[i].valid { 
+      delay(50);
+      Serial.printf("i = %d, ", i); 
+      Serial.printf("Temp(°C):%.2f, ", miThermometer.data[i].temperature / 100.0);
+      Temperature = miThermometer.data[i].temperature / 100.0; 
+      Serial.printf("RH(%%):%.2f, ", miThermometer.data[i].humidity / 100.0); 
+      Humidity = miThermometer.data[i].humidity / 100.0;
+      Serial.printf("Volt:%.3f, ", miThermometer.data[i].batt_voltage / 1000.0); 
+      Voltage = miThermometer.data[i].batt_voltage / 1000.0; 
+      char sbuff[8]; 
+      dtostrf(Voltage, 4, 3, sbuff); 
+      Serial.printf("batt(%%):%d, ", miThermometer.data[i].batt_level); 
+      Percent = miThermometer.data[i].batt_level; 
+      Serial.printf("rssi(dBm):%d\n", miThermometer.data[i].rssi); 
+      BLErssi = miThermometer.data[i].rssi;
+      if (i < 1) { 
+        t = String(Temperature);
+        t = t.substring(0, 5); 
+        h = String(Humidity);
+        h = h.substring(0, 5); 
+        v = String(sbuff);
+        v = v.substring(0, 5);
+        p = String(round(Percent));
+        p = p.substring(0, 3);
+        s = String(BLErssi);
+        if (BLErssi > -99) {
+          s = s.substring(0, 3);
+        } else {
+          s = s.substring(0, 4);
+        }
+      } else { 
+        t2 = String(Temperature);
+        t2 = t2.substring(0, 5); h2 = String(Humidity); h2 = h2.substring(0, 5); v2 = String(sbuff); v2 = v2.substring(0, 5); p2 = String(round(Percent)); p2 = p2.substring(0, 3); s2 = String(BLErssi); if (BLErssi > -99) { s2 = s2.substring(0, 3); } else { s2 = s2.substring(0, 4); } } delay(50); } } miThermometer.clearScanResults(); if (mqttClient.connected()) retries = 0; // Below lines compare the current BLE value h with previous value hp, h: humidity, t: temperature, etc... if (mqttClient.connected() and ((h.toFloat() >= hp.toFloat() * 1.01) or (h.toFloat() <= hp.toFloat() * 0.99))) { mqttClient.publish(SUB_RH, (char*)h.c_str(), true); hp = h; } if (mqttClient.connected() and ((t.toFloat() >= tp.toFloat() + 0.01)) or (t.toFloat() <= tp.toFloat() - 0.01)) { mqttClient.publish(SUB_TEMP, (char*)t.c_str(), true); tp = t; } if (mqttClient.connected() and ((v.toFloat() >= vp.toFloat() + 0.01)) or (v.toFloat() <= vp.toFloat() - 0.01)) { mqttClient.publish(SUB_V, (char*)v.c_str(), true); vp = v; } if (mqttClient.connected() and (p.toFloat() != pp.toFloat())) { mqttClient.publish(SUB_P, (char*)p.c_str(), true); pp = p; } if (mqttClient.connected() and (s.toFloat() != sp.toFloat())) { mqttClient.publish(SUB_S, (char*)s.c_str(), true); sp = s; } if (mqttClient.connected() and ((h2.toFloat() >= hp2.toFloat() * 1.01) or (h2.toFloat() <= hp2.toFloat() * 0.99))) { mqttClient.publish(SUB_RH2, (char*)h2.c_str(), true); hp2 = h2; } if (mqttClient.connected() and ((t2.toFloat() >= tp2.toFloat() + 0.01)) or (t2.toFloat() <= tp2.toFloat() - 0.01)) { mqttClient.publish(SUB_TEMP2, (char*)t2.c_str(), true); tp2 = t2; } if (mqttClient.connected() and ((v2.toFloat() >= vp2.toFloat() + 0.01)) or (v2.toFloat() <= vp2.toFloat() - 0.01)) { mqttClient.publish(SUB_V2, (char*)v2.c_str(), true); vp2 = v2; } if (mqttClient.connected() and (p2.toFloat() != pp2.toFloat())) { mqttClient.publish(SUB_P2, (char*)p2.c_str(), true); pp2 = p2; } if (mqttClient.connected() and (s2.toFloat() != sp2.toFloat())) { mqttClient.publish(SUB_S2, (char*)s2.c_str(), true); sp2 = s2; } delay(100); //delay some time for MQTT to publish and callback } }
 
 void initWifiStation() { WiFi.mode(WIFI_AP_STA); WiFi.hostname(MYHOSTNAME); WiFi.begin(ssid, password); Serial.print("\nConnecting to WiFi "); while (WiFi.status() != WL_CONNECTED) { delay(1000); Serial.print("."); retries++; if ((retries >= 10) and (retries < 20)) { if (retries == 10) { WiFi.mode(WIFI_OFF); WiFi.mode(WIFI_AP_STA); delay(500); WiFi.begin(ssid1, password1); Serial.println(String("\nTrying to Connect WiFi network (") + String(ssid1) + ")"); } } if ((retries >= 20) and (retries < 30)) { if (retries == 20) { WiFi.mode(WIFI_OFF); WiFi.mode(WIFI_AP_STA); delay(500); WiFi.begin(ssid2, password2); Serial.println(String("\nTrying to Connect WiFi network (") + String(ssid2) + ")"); } } if (retries >= 30) { ESP.restart(); } } Serial.println(String("\nConnected to the WiFi network (") + WiFi.SSID() + ")[" + WiFi.RSSI() + "]"); }
 
