b66f3758a7a534ffc47269cbf598fa9993828d0a
henrychanampd <henry.chan@ampd.energy>
2025-08-03 13:59:50 +0800

    support 3 sensors & refactor

--- ESP32-1/ESP32-1.ino
+++ ESP32-1/ESP32-1.ino
@@ -11,21 +11,23 @@ const String MYHOSTNAME = "ESP32-1TEMP";
 const char* ssid = "your_wifi_ssid1";
 const char* password = "your_wifi_password1";
 const int scanTime = 5;  // BLE scan time in seconds
-#define NUM_OF_SENSORS 2
+#define NUM_OF_SENSORS 3
 #define WDT_TIMEOUT_SECONDS 10
 
 int retries = 0;
-int loopCount = 0;
 int entries = 0;
-unsigned long PrevMillis = 0;
+unsigned long PrevSensorMillis = 0;
+unsigned long PrevGSheetMillis = 0;
+unsigned long PrevDbgPrintMillis = 0;
 typedef struct {
   String temp;
   String humidity;
-  String bat_percentage;
   String bat_volt;
+  String bat_percentage;
   String rssi;
 } SensorData_t;
 SensorData_t sensorData[NUM_OF_SENSORS];
+String data_label[] = { "temp", "rh", "bat_volt", "bat_pct", "rssi" };
 
 // Google Project ID
 #define PROJECT_ID "---"
@@ -69,16 +71,19 @@ float Percent = 0.0;
 float BLErssi = 0.0;
 
 // List of Mi sensors' BLE addresses
-std::vector<std::string> knownBLEAddresses = { "a4:c1:38:xx:xx:xx", "a4:c1:38:xx:xx:xx" };
+#define SENSOR_MAC_LIVING "a4:c1:38:xx:xx:xx"
+#define SENSOR_MAC_STUDY "a4:c1:38:xx:xx:xx"
+#define SENSOR_MAC_BED "a4:c1:38:xx:xx:xx"
+String sensor_label[NUM_OF_SENSORS] = { "Living", "Bed", "Study"};
+std::vector<std::string> knownBLEAddresses = { SENSOR_MAC_LIVING, SENSOR_MAC_BED, SENSOR_MAC_STUDY };
 ATC_MiThermometer miThermometer(knownBLEAddresses);
+bool data_ready = false;
 
 void setup() {
   Serial.begin(115200);
   initWifiStation();
   delay(500);
   miThermometer.begin();
-  loopCount = 180;
-  PrevMillis = millis();
 
   //Configure time
   configTime(0, 0, ntpServer);
@@ -106,31 +111,32 @@ void setup() {
   esp_task_wdt_add(NULL);  // Add current task (loopTask) to watchdog
 
   delay(1000);  //delay 1000ms for internet connection and timesyc to be ready
+  PrevSensorMillis = 0;
+  PrevDbgPrintMillis = 0;
+  PrevGSheetMillis = millis();
 }
 
 void loop() {
   esp_task_wdt_reset();
-  loopCount++;
-  // Check sensor data every 30,000ms
-  if (loopCount >= 200 & (millis() - PrevMillis) >= 30000) {
-    PrevMillis = millis();
+  // Check sensor data every 30s
+  if (millis() - PrevSensorMillis >= 30000) {
+    PrevSensorMillis = millis();
     miThermometer.resetData();
     unsigned found = miThermometer.getData(scanTime);
     for (int i = 0; i < miThermometer.data.size(); i++) {
       if (miThermometer.data[i].valid) {
-        delay(50);
-        Serial.printf("i = %d, ", i);
-        Serial.printf("Temp(°C):%.2f, ", miThermometer.data[i].temperature / 100.0);
+        Serial.printf("i = %d: %10s, ", i, sensor_label[i].c_str());
+        Serial.printf("%s: %.2f, ", data_label[0].c_str(), miThermometer.data[i].temperature / 100.0);
         Temperature = miThermometer.data[i].temperature / 100.0;
-        Serial.printf("RH(%%):%.2f, ", miThermometer.data[i].humidity / 100.0);
+        Serial.printf("%s: %.2f, ", data_label[1].c_str(), miThermometer.data[i].humidity / 100.0);
         Humidity = miThermometer.data[i].humidity / 100.0;
-        Serial.printf("Volt:%.3f, ", miThermometer.data[i].batt_voltage / 1000.0);
+        Serial.printf("%s: %.3f, ", data_label[2].c_str(), miThermometer.data[i].batt_voltage / 1000.0);
         Voltage = miThermometer.data[i].batt_voltage / 1000.0;
         char sbuff[8];
         dtostrf(Voltage, 4, 3, sbuff);
-        Serial.printf("batt(%%):%d, ", miThermometer.data[i].batt_level);
+        Serial.printf("%s: %3d, ", data_label[3].c_str(), miThermometer.data[i].batt_level);
         Percent = miThermometer.data[i].batt_level;
-        Serial.printf("rssi(dBm):%d\n", miThermometer.data[i].rssi);
+        Serial.printf("%s: %d\n", data_label[4].c_str(), miThermometer.data[i].rssi);
         BLErssi = miThermometer.data[i].rssi;
         sensorData[i].temp = String(Temperature);
         sensorData[i].temp = sensorData[i].temp.substring(0, 5);
@@ -146,101 +152,151 @@ void loop() {
         } else {
           sensorData[i].rssi = sensorData[i].rssi.substring(0, 4);
         }
-        delay(50);
+        data_ready = true;
       }
     }
+    Serial.println();
     miThermometer.clearScanResults();
   }
 
 
-  delay(10);
-  bool ready = GSheet.ready();
-  unsigned long locTime = getTime();
-  //Update the google sheet every 60 seconds
-  if (ready && (locTime % 60 == 0)) {
-    RxSts = false;
-    FirebaseJson response;
-    FirebaseJson valueRange;
-    offsetTime();  //subroutine to calculate local time from epoch time
-    String StrA1 = "";
-    bool GetSucess = GSheet.values.get(&response /* returned response */, spreadsheetId /* spreadsheet Id to read */, sheetTag /* range to read */);
-    // Serial.print("GetSucess res:");
-    // Serial.println(GetSucess);
-    if (GetSucess) {
-      response.toString(Serial, true);
-      response.toString(StrA1, true);
-      uint index1 = StrA1.indexOf("Time", 0);
-      StrA1 = StrA1.substring(index1, index1 + 4);
-      //Check if the sheet is blank, if blank sheet is detected, add the title on first row
-      if (StrA1 == "Time") {
-        Serial.print("GetSucess, A1 = Time");
-      } else {
-        Serial.print("GetSucess, A1 <> Time");
-        valueRange.add("majorDimension", "COLUMNS");
-        valueRange.set("values/[0]/[0]", "Time");
-        valueRange.set("values/[1]/[0]", "Temp1(C)");
-        valueRange.set("values/[2]/[0]", "RH1(%)");
-        valueRange.set("values/[3]/[0]", "Batt Lvl1(%");
-        valueRange.set("values/[4]/[0]", "Batt Volt1");
-        valueRange.set("values/[5]/[0]", "RSSI(db)");
-        valueRange.set("values/[6]/[0]", "Temp2(C)");
-        valueRange.set("values/[7]/[0]", "RH2(%)");
-        valueRange.set("values/[8]/[0]", "BattLvl2(%");
-        valueRange.set("values/[9]/[0]", "Batt Volt2");
-        valueRange.set("values/[10]/[0]", "RSSI2(db)");
-        bool success = GSheet.values.update(&response /* returned response */, spreadsheetId /* spreadsheet Id to append */, sheetTag /*"Sheet1!A1"*/ /* range to append */, &valueRange /* data range to append */);
-        if (success) {
-          response.toString(Serial, true);
-          retries = 0;
+  if (millis() - PrevGSheetMillis >= 200) {
+    PrevGSheetMillis = millis();
+    bool gsheet_ready = GSheet.ready();
+    unsigned long locTime = getTime();
+    //Update the google sheet every 60 seconds
+    if (data_ready && gsheet_ready && (locTime % 60 == 0)) {
+      data_ready = false;
+      RxSts = false;
+      FirebaseJson response;
+      FirebaseJson valueRange;
+      offsetTime();  //subroutine to calculate local time from epoch time
+      String StrA1 = "";
+      bool GetSucess = GSheet.values.get(&response /* returned response */, spreadsheetId /* spreadsheet Id to read */, sheetTag /* range to read */);
+      // Serial.print("GetSucess res:");
+      // Serial.println(GetSucess);
+      if (GetSucess) {
+        // response.toString(Serial, true);
+        // response.toString(StrA1, true);
+        uint index1 = StrA1.indexOf("Time", 0);
+        StrA1 = StrA1.substring(index1, index1 + 4);
+        //Check if the sheet is blank, if blank sheet is detected, add the title on first row
+        if (StrA1 == "Time") {
+          Serial.println("GetSucess, A1 = Time");
         } else {
-          Serial.println(GSheet.errorReason());
-          retries++;
-          if (retries >= 3) {
-            ESP.restart();
+          String label;
+          Serial.println("GetSucess, A1 <> Time");
+          valueRange.add("majorDimension", "COLUMNS");
+          valueRange.set("values/[0]/[0]", "Time");
+          // sensor 0 temperature
+          label = sensor_label[0] + String("") + data_label[0];
+          valueRange.set("values/[1]/[0]", label.c_str());
+          // sensor 0 humidity
+          label = sensor_label[0] + String("") + data_label[1];
+          valueRange.set("values/[2]/[0]", label.c_str());
+          // sensor 0 battery percentage
+          label = sensor_label[0] + String("") + data_label[2];
+          valueRange.set("values/[3]/[0]", label.c_str());
+          // sensor 0 battery voltage
+          label = sensor_label[0] + String("") + data_label[3];
+          valueRange.set("values/[4]/[0]", label.c_str());
+          // sensor 0 RSSI
+          label = sensor_label[0] + String("") + data_label[4];
+          valueRange.set("values/[5]/[0]", label.c_str());
+          // sensor 1 temperature
+          label = sensor_label[1] + String("") + data_label[0];
+          valueRange.set("values/[6]/[0]", label.c_str());
+          // sensor 1 humidity
+          label = sensor_label[1] + String("") + data_label[1];
+          valueRange.set("values/[7]/[0]", label.c_str());
+          // sensor 1 battery percentage
+          label = sensor_label[1] + String("") + data_label[2];
+          valueRange.set("values/[8]/[0]", label.c_str());
+          // sensor 1 battery voltage
+          label = sensor_label[1] + String("") + data_label[3];
+          valueRange.set("values/[9]/[0]", label.c_str());
+          // sensor 1 RSSI
+          label = sensor_label[1] + String("") + data_label[4];
+          valueRange.set("values/[10]/[0]", label.c_str());
+          // sensor 2 temperature
+          label = sensor_label[2] + String("") + data_label[0];
+          valueRange.set("values/[11]/[0]", label.c_str());
+          // sensor 2 humidity
+          label = sensor_label[2] + String("") + data_label[1];
+          valueRange.set("values/[12]/[0]", label.c_str());
+          // sensor 2 battery percentage
+          label = sensor_label[2] + String("") + data_label[2];
+          valueRange.set("values/[13]/[0]", label.c_str());
+          // sensor 2 battery voltage
+          label = sensor_label[2] + String("") + data_label[3];
+          valueRange.set("values/[14]/[0]", label.c_str());
+          // sensor 2 RSSI
+          label = sensor_label[2] + String("") + data_label[4];
+          valueRange.set("values/[15]/[0]", label.c_str());
+          bool success = GSheet.values.update(&response /* returned response */, spreadsheetId /* spreadsheet Id to append */, sheetTag /*"Sheet1!A1"*/ /* range to append */, &valueRange /* data range to append */);
+          if (success) {
+            // response.toString(Serial, true);
+            retries = 0;
+          } else {
+            Serial.println(GSheet.errorReason());
+            retries++;
+            if (retries >= 3) {
+              ESP.restart();
+            }
           }
+          //Show the current heap state of ESP32 for debug
+          //Serial.print("Size:");
+          //Serial.print(ESP.getHeapSize());
+          //Serial.print(", Free:");
+          //Serial.print(ESP.getFreeHeap());
+          //Serial.print(", Min:");
+          //Serial.println(ESP.getMinFreeHeap());
         }
-        //Show the current heap state of ESP32 for debug
-        //Serial.print("Size:");
-        //Serial.print(ESP.getHeapSize());
-        //Serial.print(", Free:");
-        //Serial.print(ESP.getFreeHeap());
-        //Serial.print(", Min:");
-        //Serial.println(ESP.getMinFreeHeap());
+      }
+      //Prepare the Google sheet values to fit a ROW
+      valueRange.add("majorDimension", "COLUMNS");
+      valueRange.set("values/[0]/[0]", TimeStamp);
+      valueRange.set("values/[1]/[0]", sensorData[0].temp);
+      valueRange.set("values/[2]/[0]", sensorData[0].humidity);
+      valueRange.set("values/[3]/[0]", sensorData[0].bat_percentage);
+      valueRange.set("values/[4]/[0]", sensorData[0].bat_volt);
+      valueRange.set("values/[5]/[0]", sensorData[0].rssi);
+      valueRange.set("values/[6]/[0]", sensorData[1].temp);
+      valueRange.set("values/[7]/[0]", sensorData[1].humidity);
+      valueRange.set("values/[8]/[0]", sensorData[1].bat_percentage);
+      valueRange.set("values/[9]/[0]", sensorData[1].bat_volt);
+      valueRange.set("values/[10]/[0]", sensorData[1].rssi);
+      valueRange.set("values/[11]/[0]", sensorData[2].temp);
+      valueRange.set("values/[12]/[0]", sensorData[2].humidity);
+      valueRange.set("values/[13]/[0]", sensorData[2].bat_percentage);
+      valueRange.set("values/[14]/[0]", sensorData[2].bat_volt);
+      valueRange.set("values/[15]/[0]", sensorData[2].rssi);
+      entries++;
+      //Append row data to Google sheet (create new row)
+      bool success = GSheet.values.append(&response /* returned response */, spreadsheetId /* spreadsheet Id to append */, sheetTag /* range to append */, &valueRange /* data range to append */);
+      if (success) {
+        // response.toString(Serial, true);
+      } else {
+        Serial.println(GSheet.errorReason());
+        retries++;
+        if (retries >= 3) {
+          ESP.restart();
+        }
       }
     }
-    //Prepare the Google sheet values to fit a ROW
-    valueRange.add("majorDimension", "COLUMNS");
-    valueRange.set("values/[0]/[0]", TimeStamp);
-    valueRange.set("values/[1]/[0]", sensorData[0].temp);
-    valueRange.set("values/[2]/[0]", sensorData[0].humidity);
-    valueRange.set("values/[3]/[0]", sensorData[0].bat_percentage);
-    valueRange.set("values/[4]/[0]", sensorData[0].bat_volt);
-    valueRange.set("values/[5]/[0]", sensorData[0].rssi);
-    valueRange.set("values/[6]/[0]", sensorData[1].temp);
-    valueRange.set("values/[7]/[0]", sensorData[1].humidity);
-    valueRange.set("values/[8]/[0]", sensorData[1].bat_percentage);
-    valueRange.set("values/[9]/[0]", sensorData[1].bat_volt);
-    valueRange.set("values/[10]/[0]", sensorData[1].rssi);
-    entries++;
-    //Append row data to Google sheet (create new row)
-    bool success = GSheet.values.append(&response /* returned response */, spreadsheetId /* spreadsheet Id to append */, sheetTag /* range to append */, &valueRange /* data range to append */);
-    if (success) {
-      response.toString(Serial, true);
-    } else {
-      Serial.println(GSheet.errorReason());
-      retries++;
-      if (retries >= 3) {
-        ESP.restart();
-      }
-    }
-    //Show the current heap state of ESP32 for debug
-    //Serial.print("Size:");
-    //Serial.print(ESP.getHeapSize());
-    //Serial.print(", Free:");
-    //Serial.print(ESP.getFreeHeap());
-    //Serial.print(", Min:");
-    //Serial.println(ESP.getMinFreeHeap());
   }
+  if (millis() - PrevDbgPrintMillis >= 300000) {
+    // print the heap usage every 5 minutes
+    PrevDbgPrintMillis = millis();
+    //Show the current heap state of ESP32 for debug
+    Serial.print("Size:");
+    Serial.print(ESP.getHeapSize());
+    Serial.print(", Free:");
+    Serial.print(ESP.getFreeHeap());
+    Serial.print(", Min:");
+    Serial.println(ESP.getMinFreeHeap());
+  }
+  delay(100);
 }
 
 void initWifiStation() {
@@ -260,7 +316,10 @@ void initWifiStation() {
       }
     }
   }
-  Serial.println(String("\nConnected to the WiFi network (") + WiFi.SSID() + ")[" + WiFi.RSSI() + "]");
+  WiFi.setTxPower(WIFI_POWER_2dBm);
+  delay(500);
+  int currentPower = WiFi.getTxPower();
+  Serial.println(String("\nConnected to the WiFi network (") + WiFi.SSID() + ")[" + WiFi.RSSI() + "], TX power: " + String(currentPower) + " dBm");
 }
 
 void offsetTime() {
@@ -304,10 +363,10 @@ void offsetTime() {
   }
   sprintf(TimeStamp, "%04d-%02d-%02d %02d:%02d:%02d", yy, mm, dd, hh, timeinfo.tm_min, timeinfo.tm_sec);
   sprintf(sheetTag, "%04d%02d!A1", yy, mm);
-  Serial.print("TimeStamp = ");
-  Serial.print(TimeStamp);
-  Serial.print(" , sheetTag = ");
-  Serial.println(sheetTag);
+  // Serial.print("TimeStamp = ");
+  // Serial.print(TimeStamp);
+  // Serial.print(" , sheetTag = ");
+  // Serial.println(sheetTag);
 }
 
 void tokenStatusCallback(TokenInfo info) {
